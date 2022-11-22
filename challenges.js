45290ddcbf11c05128b3787a8c1640d0c24047b9
Bantealem Geto <bantem2021@gmail.com>
2022-11-22 10:24:04 +0300

    Create challenges.js

---
+++ challenges.js
@@ -0,0 +1,16 @@
+function miniMaxSum(arr) {
+    // Write your code here
+const sortedarray = arr.sort();
+let min_sum = 0;
+let max_sum = 0;
+for(let i=0; i<arr.length; i++) {
+    if(i<arr.length - 1) {
+        min_sum += arr[i]
+    }
+    if(i>0) {
+        max_sum += arr[i]
+    }
+}
+console.log(min_sum, max_sum)
+    
+}
